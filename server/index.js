a3f58e66360552234a9e6b47276c204827ae7083
Rian Brunelli <rian10brunelli@gmail.com>
2023-07-03 16:04:06 -0300

    authentication and authorization

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
 /* configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +35,12 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage });
+
+/* routes with files */
+app.post("auth/register", upload.single("picture"), register);
+
+/* routes */
+app.use("/auth", authRoutes);
 
 /* mongoose setup */
 const PORT = process.env.PORT || 3000;
